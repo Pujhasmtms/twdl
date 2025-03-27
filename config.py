6f5b1a1e6ab5ad88c7d8f305f5474ff44273b6a7
Pujhasmtms <dhpujha@gmail.com>
2025-03-27 07:19:21 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # Your telegram bot token
-DEVELOPER_ID = 1234567890  # Your telegram id (used for error reporting and private mode)
+BOT_TOKEN = "7713731153:AAGBAXe85z510cUiiL7623VOC0EeGRqo2Gs"  # Your telegram bot token
+DEVELOPER_ID = 880184388  # Your telegram id (used for error reporting and private mode)
 IS_BOT_PRIVATE = False  # Change this to True to make bot private
